ddb4797d642a9b81f6983243b0bcecc1098c477c
DanteSparda <dantesparda@abv.bg>
2015-02-19 17:46:44 +0200

    Timer Added

---
+++ TheMaze/StartMap/StartGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Molq vsi4ki da commitnat 1 komentar za da znaem, 4e sme svurzani
+            //Testing from Vladi
+
+            Console.WriteLine("{0}", "Testing from Stan");
+            //testing sync  Mihail
+            // test Rado
+            // test Svetli
+            //Test Iliana 23
+            //Opravih taimera za vratata i za igrata (a e ve4niq taimer koit oshte e za highscore, b e timera za vratite. Namirat se v GameClock)
+            GameClock a = new GameClock();
+            GameClock b = new GameClock();
+            a.GameClockDisplay();
+            b.GameTimerDisplay();
+
+        }
+    }
+}
